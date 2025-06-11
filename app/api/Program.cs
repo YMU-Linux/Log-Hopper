79e1810b4ae8c2db880a8772d4bec57e207ed1ba
muy <yannick.mueller2@EDU.LAN>
2025-06-11 11:33:32 +0200

    Database and Doc update

--- app/api/Program.cs
+++ app/api/Program.cs
@@ -28,7 +28,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseHttpsRedirection();
